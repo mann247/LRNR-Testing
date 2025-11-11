e112e8733c3322e0ea03dfd729f8a93b003c9549
Jhoana Peralta <jperalta17@r2happrentice.org>
2025-11-11 11:45:42 -0500

    static results page

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import '../styles/Results.css';
 
 export default function Results() {
-    return <h1>results page</h1>;
+    return (
+        <div className='results'>
+            <h1 className='rtitle'>lrnr</h1>
+            <h2 className='rresults'>Questions Right: 0111</h2>
+            <Link to='/quiz'>
+             <button className='tbtn'>Try Another Quiz</button>
+            </Link>
+        </div>
+    );
 }
